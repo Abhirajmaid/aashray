8079a8f29d1e1e34a018494c0f60e893d40d0586
TheAbhiraj <codersground404@gmail.com>
2023-08-05 07:29:07 +0530

    changes

--- aashray/src/utils/api.js
+++ aashray/src/utils/api.js
@@ -4,7 +4,7 @@ export const fetchDataFromApi = async (endpoint) => {
     const options = {
         method: 'GET',
         headers: {
-            Authorization: 'Bearer b4e8678c7ed32e303aa3672107f93a19264ce0c1b72dac5c7fe790ff52656e82688f2d96dc56b126321d0ba2bc0a433a6bebf0e1827ee2da92f5fe1455732d24a3fa27080c154a6231525a11f970d8d695d813400afab728790c454139c16c5f03d7d123bd20cf7a089de83533fce0d2d62bd31afedb9d22becf2661e7cc11d5'
+            Authorization: 'Bearer db407e3b1b79c45acb4897bbbe6edf7c54c843ebf56721d23b08c061b1673533e2b46f7210644f760bacd08256424fd3797edae4c1f290d8637ce447404e994bb315f1fbafebdc0a717e0df4bce17bca97b71c306e0f2b3b0dba8f8614ea6d1a1027fc166060caff7d4f4685665848dd21a9f23dcae5ac9045fcf93cc9acce8e'
         }
     };
 
